dadcaac37e89c1b8c185fa57677b8c07675d26e6
Nick Lanng <rinse@rinseworld.com>
2022-12-14 10:34:53 +0000

    Day 14

---
+++ day14/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"fmt"
+	"github.com/nicklanng/aoc22/lib"
+	"strings"
+)
+
+const width, height = 800, 200
+
+const (
+	Air byte = iota
+	Rock
+	Sand
+)
+
+//go:embed input
+var input []byte
+
+func main() {
+	var (
+		tiles     [width * height]byte
+		sandCount int
+	)
+
+	// part 1
+	tiles = parseInput(false)
+	for {
+		sandCount++
+		if !simulate(tiles[:]) {
+			break
+		}
+	}
+	fmt.Printf("Part 1 output: %d\n", sandCount-1)
+
+	// part 2
+	tiles = parseInput(true)
+	sandCount = 0
+	for {
+		sandCount++
+		if !simulate(tiles[:]) {
+			break
+		}
+	}
+
+	fmt.Printf("Part 2 output: %d\n", sandCount)
+}
+
+func simulate(tiles []byte) bool {
+	point := 500
+
+	for {
+		if point+width > len(tiles) {
+			return false
+		}
+
+		// try to move down
+		if tiles[point+width] == Air {
+			point += width
+			continue
+		}
+
+		// try to move down left
+		if tiles[point+width-1] == Air {
+			point += width - 1
+			continue
+		}
+		// try to move down right
+		if tiles[point+width+1] == Air {
+			point += width + 1
+			continue
+		}
+
+		tiles[point] = Sand
+		return point != 500
+	}
+}
+
+func parseInput(drawFloor bool) [width * height]byte {
+	var tiles [width * height]byte
+	var highestY int
+
+	scanner := bufio.NewScanner(bytes.NewReader(input))
+
+	for scanner.Scan() {
+		// read line
+		var points []lib.Vector2i
+		pointsStr := strings.Split(scanner.Text(), " -> ")
+		for _, str := range pointsStr {
+			var point lib.Vector2i
+			if err := point.ParseString(str); err != nil {
+				panic(err)
+			}
+			points = append(points, point)
+		}
+
+		// draw rocks
+		for i := 0; i < len(points)-1; i++ {
+			start := points[i]
+			end := points[i+1]
+
+			if start.Y > highestY {
+				highestY = start.Y
+			}
+
+			if start.Y == end.Y {
+				// horizontal
+				if end.X < start.X {
+					end, start = start, end
+				}
+				for ; start.X <= end.X; start.X++ {
+					tiles[start.ToIndex(width)] = Rock
+				}
+			} else {
+				// vertical
+				if end.Y < start.Y {
+					end, start = start, end
+				}
+				for ; start.Y <= end.Y; start.Y++ {
+					tiles[start.ToIndex(width)] = Rock
+				}
+			}
+		}
+	}
+
+	if drawFloor {
+		for x := 0; x <= width; x++ {
+			tiles[(highestY+2)*width+x] = Rock
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return tiles
+}
--- lib/vectors.go
+++ lib/vectors.go
@@ -1,12 +1,17 @@
 package lib
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type Vector2i struct{ X, Y int }
-func (v Vector2i) Add(v2 Vector2i) Vector2i {return Vector2i{X: v.X+v2.X, Y: v.Y+v2.Y}}
-func (v Vector2i) Sub(v2 Vector2i) Vector2i {return Vector2i{X: v.X-v2.X, Y: v.Y-v2.Y}}
-func (v Vector2i) Magnitude() float64               {return math.Sqrt(float64(v.X*v.X + v.Y*v.Y))}
-func (v Vector2i) Distance(v2 Vector2i) float64 {return v.Sub(v2).Magnitude()}
+
+func (v Vector2i) ToIndex(width int) int        { return v.Y*width + v.X }
+func (v Vector2i) Add(v2 Vector2i) Vector2i     { return Vector2i{X: v.X + v2.X, Y: v.Y + v2.Y} }
+func (v Vector2i) Sub(v2 Vector2i) Vector2i     { return Vector2i{X: v.X - v2.X, Y: v.Y - v2.Y} }
+func (v Vector2i) Magnitude() float64           { return math.Sqrt(float64(v.X*v.X + v.Y*v.Y)) }
+func (v Vector2i) Distance(v2 Vector2i) float64 { return v.Sub(v2).Magnitude() }
 func (v Vector2i) UnitNormalize() Vector2i {
 	if v.X != 0 {
 		v.X = v.X / IntAbs(v.X)
@@ -15,4 +20,8 @@ func (v Vector2i) UnitNormalize() Vector2i {
 		v.Y = v.Y / IntAbs(v.Y)
 	}
 	return v
+}
+func (v *Vector2i) ParseString(s string) error {
+	_, err := fmt.Sscanf(s, "%d,%d", &v.X, &v.Y)
+	return err
 }
