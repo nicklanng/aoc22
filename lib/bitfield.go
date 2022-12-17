5822c24ce3564d0559796ea0f074f872b05f89fe
Nick Lanng <rinse@rinseworld.com>
2022-12-17 18:44:44 +0000

    Add bitfield to lib

--- lib/bitfield.go
+++ lib/bitfield.go
@@ -1,1 +1,31 @@
 package lib
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Bitfield1[T Unsigned] struct {
+	Raw T
+}
+
+func NewBitfield1[T Unsigned](value T) Bitfield1[T] {
+	return Bitfield1[T]{
+		Raw: value,
+	}
+}
+
+func (b *Bitfield1[T]) Set(i int) {
+	b.Raw |= 1 << i
+}
+
+func (b *Bitfield1[T]) Clear(i int) {
+	b.Raw &^= 1 << i
+}
+
+func (b *Bitfield1[T]) Toggle(i int) {
+	b.Raw ^= 1 << i
+}
+
+func (b *Bitfield1[T]) Has(i int) bool {
+	return b.Raw&(1<<i) != 0
+}
